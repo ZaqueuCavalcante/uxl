a7414a6c9b737a9340144fca8e0c961140e920a2
ZaqueuCavalcante <zaqueudovale@gmail.com>
2024-03-21 22:07:10 -0300

    Fix tests

--- Back/Configs/RateLimiterConfigs.cs
+++ Back/Configs/RateLimiterConfigs.cs
@@ -13,7 +13,7 @@ public static class RateLimiterConfigs
 
             if (Env.IsTesting() || Env.IsDevelopment())
             {
-                options.AddFixedWindowLimiter("Small", o => { o.PermitLimit = 1000; o.Window = TimeSpan.FromHours(1); });
+                options.AddFixedWindowLimiter("Small", o => { o.PermitLimit = 10_000; o.Window = TimeSpan.FromHours(1); });
                 return;
             }
 
